3795604dc968bbbbf6bde8c0a73e1a9e6a7e69a9
ESJBRE1 <123552275+ESJBRE1@users.noreply.github.com>
2023-11-12 18:12:41 +0000

    Adding an initial test

---
+++ src/test/java/com/atradius/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atradius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
